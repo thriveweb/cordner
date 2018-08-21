5871e4fb6d8e396d67719c114b61640402b9a22d
Francesca <francesca@thriveweb.com.au>
2018-08-21 10:08:45 +1000

    make hamburger close after click

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -7,26 +7,70 @@ import './Hamburger.css'
 import { slide as Menu } from 'react-burger-menu'
 
 class Hamburger extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hideServicesSubmenu: true,
+      hideMobileMenu: true,
+      menuOpen: false
+    }
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen })
+  }
+
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu() {
+    this.setState({ menuOpen: false })
+  }
+
   showSettings(event) {
     event.preventDefault()
   }
 
+  toggleServicesSubmenu() {
+    if (this.state.hideServicesSubmenu) {
+      this.setState({ hideServicesSubmenu: false })
+    } else {
+      this.setState({ hideServicesSubmenu: true })
+    }
+  }
+
+  hideSubMenu() {
+    this.setState({ menuOpen: false })
+  }
+
+  closeMobileMenu() {}
+
   render() {
+    let state = this.state
+
     return (
-      <Menu right>
-        <NavLink to="/our-services/" exact>
+      <Menu
+        isOpen={this.state.menuOpen}
+        onStateChange={state => this.handleStateChange(state)}
+        right
+      >
+        <NavLink to="/" onClick={this.hideSubMenu.bind(this)} exact>
+          Home
+        </NavLink>
+        <NavLink to="/our-services/" onClick={this.hideSubMenu.bind(this)}>
           Our Services
         </NavLink>
-        <NavLink to="/advisors/" exact>
+        <NavLink to="/advisors/" onClick={this.hideSubMenu.bind(this)} exact>
           Your Advisors
         </NavLink>
-        <NavLink to="/resources/" exact>
+        <NavLink to="/resources/" onClick={this.hideSubMenu.bind(this)} exact>
           Resources
         </NavLink>
-        <NavLink to="/blog/" exact>
+        <NavLink to="/blog/" onClick={this.hideSubMenu.bind(this)} exact>
           Blog
         </NavLink>
-        <NavLink to="/contact/" exact>
+        <NavLink to="/contact/" onClick={this.hideSubMenu.bind(this)} exact>
           Contact
         </NavLink>
 
