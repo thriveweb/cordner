f173b8521d049c24ae67ced1dcb222ba7c427a42
Francesca <francesca@thriveweb.com.au>
2018-08-21 15:47:15 +1000

    Section--1: development, styling, responsive

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
@@ -12,16 +13,57 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
       <PageHeader large title={title} backgroundImage={featuredImage} />
 
       <div className="container">
-        <div className="button">
+        <Link to="/" className="button">
           <p>Let’s talk business</p>
           <img src="/images/uploads/button_arrow.svg" alt="login icon" />
-        </div>
+        </Link>
       </div>
     </div>
 
-    <section className="section">
+    <section className="section--1 section">
       <div className="container">
-        <Content source={body} />
+        <div className="flex-column">
+          <div>
+            <p className="numbers">300+</p>
+            <p className="client">Happy clients</p>
+          </div>
+
+          <div>
+            <h3>Delivering trustworthy financial solutions since 1981</h3>
+          </div>
+
+          <div className="about-us">
+            <Link className="flex" to="/">
+              <p className="big-body">More about us</p>
+              <img src="/images/uploads/button_arrow_1.svg" alt="login icon" />
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex-column">
+          <div>
+            <span className="tagline opacity">01 </span>
+            <span className="tagline">About Us</span>
+          </div>
+
+          <div>
+            <h4>Versatile financial solutions for everyone</h4>
+            <p className="opacity">
+              Established on the Gold Coast in 1981. Cordner Advisory
+              specialises in Business Advisory Services, Tax & Compliance,
+              Family Wealth & Superannuation (SMSF), and specialist R&D tax
+              incentive/government grants.
+            </p>
+
+            <p className="opacity">
+              Cordner Advisory service a broad range of businesses and most
+              industries including technology companies and startups, childcare,
+              medical/health, construction and property. Cordner Advisory
+              delivers experienced senior big firm specialist skills with local
+              care and attention.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   </main>
