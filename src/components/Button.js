7703afac8f24d50a8c1046e29313ffca92d4d190
Francesca <francesca@thriveweb.com.au>
2018-08-24 10:49:14 +1000

    create Button.js component

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import Image from './Image'
+
+import './SingleService.scss'
+
+export default ({ link, title }) => (
+  <div className="button--secondary">
+    <Link className="flex" to={link}>
+      <p className="big-body">{title}</p>
+      <img src="/images/uploads/button_arrow_1.svg" alt="login icon" />
+    </Link>
+  </div>
+)
