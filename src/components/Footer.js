6946658b0688bfdcb286dc2a8a103212906598bb
Francesca <francesca@thriveweb.com.au>
2018-08-24 15:01:16 +1000

    begin "Footer.js"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-import './Footer.css'
+import './Footer.scss'
 
 export default ({ globalSettings, socialSettings, navLinks }) => (
-  <footer className='Footer'>
-    <div className='container taCenter'>
-      <span>© 2017 All rights reserved.</span>
+  <footer className="Footer">
+    <div className="container section flex">
+      <div className="column taLeft">
+        <h3>Contact Us</h3>
+      </div>
+
+      <div className="column taCenter">
+        <h3>Sign up for our newsletter</h3>
+      </div>
+
+      <div className="column taRight">
+        <h3>Info></h3>
+      </div>
     </div>
   </footer>
 )
