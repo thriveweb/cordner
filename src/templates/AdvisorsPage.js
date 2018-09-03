ae9f4d1501685a06475eee8d763ec7c86ff3ad8e
Francesca <francesca@thriveweb.com.au>
2018-09-03 15:51:50 +1000

    Code Section--2 (base code before component)

--- src/templates/AdvisorsPage.js
+++ src/templates/AdvisorsPage.js
@@ -5,6 +5,7 @@ import PageHeader from '../components/PageHeader'
 import Image from '../components/Image'
 import Content from '../components/Content.js'
 import NumberedHeader from '../components/NumberedHeader'
+import Link from 'gatsby-link'
 
 import './AdvisorsPage.scss'
 
@@ -34,6 +35,148 @@ export const AdvisorsPageTemplate = ({ title, subtitle, featuredImage }) => (
             industries including technology companies and startups, childcare,
             medical/health, construction and property. Cordner Advisory delivers
           </p>
+        </div>
+      </div>
+    </section>
+
+    <section className="section--2">
+      <div className="grid">
+        <Link to="" className="single--team relative">
+          <Image
+            background
+            src="/images/uploads/team-member--1.jpg"
+            alt={title}
+            size="cover"
+          />
+
+          <div className="sneak-peak cream">
+            <div className="sneak-peak--text">
+              <h3>Di Girvin</h3>
+              <p>
+                Small to Medium sized Business Advisory and R&D Legislation
+                Specialist
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="" className="single--team relative">
+          <Image
+            background
+            src="/images/uploads/team-member--2.jpg"
+            alt={title}
+            size="cover"
+          />
+
+          <div className="sneak-peak grey">
+            <div className="sneak-peak--text">
+              <h3>Di Girvin</h3>
+              <p>
+                Small to Medium sized Business Advisory and R&D Legislation
+                Specialist
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="" className="single--team relative">
+          <Image
+            background
+            src="/images/uploads/team-member--3.jpg"
+            alt={title}
+            size="cover"
+          />
+          <div className="sneak-peak purple">
+            <div className="sneak-peak--text">
+              <h3>Di Girvin</h3>
+              <p>
+                Small to Medium sized Business Advisory and R&D Legislation
+                Specialist
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="" className="single--team relative">
+          <Image
+            background
+            src="/images/uploads/team-member--4.jpg"
+            alt={title}
+            size="cover"
+          />
+
+          <div className="sneak-peak red">
+            <div className="sneak-peak--text">
+              <h3>Di Girvin</h3>
+              <p>
+                Small to Medium sized Business Advisory and R&D Legislation
+                Specialist
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="" className="single--team relative">
+          <Image
+            background
+            src="/images/uploads/team-member--5.jpg"
+            alt={title}
+            size="cover"
+          />
+
+          <div className="sneak-peak brown">
+            <div className="sneak-peak--text">
+              <h3>Di Girvin</h3>
+              <p>
+                Small to Medium sized Business Advisory and R&D Legislation
+                Specialist
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="" className="single--team relative">
+          <Image
+            background
+            src="/images/uploads/team-member--6.jpg"
+            alt={title}
+            size="cover"
+          />
+
+          <div className="sneak-peak cream">
+            <div className="sneak-peak--text">
+              <h3>Di Girvin</h3>
+              <p>
+                Small to Medium sized Business Advisory and R&D Legislation
+                Specialist
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="" className="single--team relative">
+          <Image
+            background
+            src="/images/uploads/team-member--7.jpg"
+            alt={title}
+            size="cover"
+          />
+
+          <div className="sneak-peak grey">
+            <div className="sneak-peak--text">
+              <h3>Di Girvin</h3>
+              <p>
+                Small to Medium sized Business Advisory and R&D Legislation
+                Specialist
+              </p>
+            </div>
+          </div>
+        </Link>
+        <div className="services-sneak ">
+          <div className="container">
+            <h2>Browse our services</h2>
+            <p className="big-body">
+              Maecenas consectetur mi lacus, sit amet facilisis elit dignissim
+              vitae. Nam dictum ac sapien vitae porta.{' '}
+            </p>
+            <Link to="our-services" className="Button">
+              KNOW MORE
+            </Link>
+          </div>
         </div>
       </div>
     </section>
