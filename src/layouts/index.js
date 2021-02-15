c368bda91eb3f4449c4f916c5ebd21a68f2b4460
deanoakley <dean@thriveweb.com.au>
2021-02-15 10:40:16 +1000

    new fonts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,36 +18,39 @@ export default ({ children, data }) => {
     <Fragment>
       <Helmet defaultTitle={siteTitle} titleTemplate={`%s | ${siteTitle}`}>
         <link
-          href="https://fonts.googleapis.com/css?family=Lora"
-          rel="stylesheet"
-        />
-        <link
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
           integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
           crossorigin="anonymous"
         />
 
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
         {/* Global site tag (gtag.js) - Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134430735-1"></script>
-        <script>{`
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-134430735-1"
+        />
+        <script>
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', 'UA-134430735-1');
         `}
         </script>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
-new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
-’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,‘script’,‘dataLayer’,‘GTM-5WHHNSW’);</script>
-<!-- End Google Tag Manager -->Additionally, paste this code immediately after the opening <body> tag:<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src=“https://www.googletagmanager.com/ns.html?id=GTM-5WHHNSW”
-height=“0" width=“0” style=“display:none;visibility:hidden”></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+        <script>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
+          new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
+          ’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,‘script’,‘dataLayer’,‘GTM-5WHHNSW’);
+        `}
+        </script>
       </Helmet>
 
       <Meta
